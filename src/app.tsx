70deeb2f411f36198a39d5250c0e99d34b21712b
lovrozagar <lovro.zagar5@gmail.com>
2024-10-20 01:20:51 +0200

    feat(query provider): wrap app with query client provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 import '@/style/globals.css'
+import { queryClient } from '@/utils'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
-	return <div />
+	return (
+		<QueryClientProvider client={queryClient}>
+			<div />
+		</QueryClientProvider>
+	)
 }
 
 export { App }
