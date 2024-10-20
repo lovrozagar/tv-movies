b112c6bbd6be355abc6b156d8909e496a71f9282
lovrozagar <lovro.zagar5@gmail.com>
2024-10-21 01:26:43 +0200

    feat(query): add category movie list query hook

---
+++ src/hook/query/useCategoryMovieListQuery/useCategoryMovieListQuery.ts
@@ -0,0 +1,28 @@
+import type { ApiResponse, MovieItem, Page } from '@/api'
+import { fetchQuery, getApiPath, getQueryKey } from '@/utils'
+import { useInfiniteQuery } from '@tanstack/react-query'
+
+type UseCategoryMovieListQueryProps = {
+	genreId: number
+}
+
+function useCategoryMovieListQuery(props: UseCategoryMovieListQueryProps) {
+	const { genreId } = props
+
+	return useInfiniteQuery({
+		initialPageParam: 1,
+		queryKey: [getQueryKey('category'), genreId],
+		queryFn: async ({ pageParam }) => {
+			return await fetchQuery<ApiResponse<Page<MovieItem>>>({
+				path: getApiPath(`/discover/movie?with_genres=${genreId}&page=${pageParam}`),
+			})
+		},
+		getNextPageParam: (data) => {
+			const isLastPage = data.page === data.total_pages
+
+			return isLastPage ? undefined : data.page + 1
+		},
+	})
+}
+
+export { useCategoryMovieListQuery }
