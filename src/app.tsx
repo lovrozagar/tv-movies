69beacf94876fd2dc1aa8d6205d9875096f2a5de
lovrozagar <lovro.zagar5@gmail.com>
2024-10-20 16:23:36 +0200

    feat(spatial navigation): config & init

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,20 @@
+import { Navbar } from '@/module/navbar/component/navbar'
 import '@/style/globals.css'
 import { queryClient } from '@/utils'
+import { init } from '@noriginmedia/norigin-spatial-navigation'
 import { QueryClientProvider } from '@tanstack/react-query'
+
+/* init spatial navigation */
+init({
+	debug: false,
+	visualDebug: false,
+	distanceCalculationMethod: 'center',
+})
 
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<div />
+			<Navbar />
 		</QueryClientProvider>
 	)
 }
