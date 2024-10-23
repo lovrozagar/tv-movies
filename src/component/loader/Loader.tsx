352b44a63e9f09528d76dadf6cafc5789d9bf29d
lovrozagar <lovro.zagar5@gmail.com>
2024-10-23 03:09:26 +0200

    feat(component): loader

---
+++ src/component/loader/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+	return (
+		<div className='flex w-full flex-1 items-center justify-center bg-background'>
+			<span className='size-6 animate-[spin_800ms_linear_infinite] rounded-full border-foreground border-t-[2px] border-r-[2px]' />
+		</div>
+	)
+}
+
+export { Loader }
