39a121944e0fa3d51dfaa049f836531601772c65
lovrozagar <lovro.zagar5@gmail.com>
2024-10-23 03:43:40 +0200

    feat(screen): home

--- src/screen/home/HomeScreen.tsx
+++ src/screen/home/HomeScreen.tsx
@@ -1,59 +1,25 @@
-import { StarIcon } from '@/component/icon/star-icon'
-import { Image } from '@/component/image'
-import { Link } from '@/component/link'
-import { useCategoryMovieListQuery } from '@/hook/query'
-import { BackNavigator } from '@/module/renderless/BackNavigator'
-import { TrackHeading } from '@/module/track/component/TrackHeading'
-import { FocusProvider } from '@/provider'
-import { getImageSource } from '@/utils'
+import { HOME_GENRE_LIST } from '@/constants'
+import { Track } from '@/module/track'
+import { HomeTrack } from '@/screen/home/components/HomeTrack'
 
 const HomeScreen = () => {
-	const recommendedResponse = useCategoryMovieListQuery({ genreId: 36 })
-
-	const results = recommendedResponse.data?.pages.at(-1)?.results
-
 	return (
-		<>
-			<BackNavigator />
-
-			<main className='px-4 py-2'>
-				<div className='mt-2 text-mode-500 text-sm uppercase'>Home</div>
-				<TrackHeading>Recommended</TrackHeading>
-				<FocusProvider>
-					{({ ref }) => (
-						<div ref={ref} className='mt-3 flex gap-x-4'>
-							{results?.map((result, index) => (
-								<li
-									key={result.id}
-									className='flex min-w-[200px] flex-1 items-stretch justify-stretch'
-								>
-									<Link
-										key={result.id}
-										autoFocus={index === 0}
-										to={`/movie/${result.id}`}
-										className='flex w-[200px] flex-1 flex-col items-stretch justify-stretch hover:bg-transparent data-[focused=true]:bg-transparent'
-									>
-										<div className='relative'>
-											<Image
-												effect
-												src={getImageSource({ size: 'w342', path: result.poster_path })}
-												alt={result.title}
-											/>
-											<div className='absolute right-2 bottom-2 flex items-center gap-x-1 rounded-full bg-mode px-2 py-1 text-[0.65rem]'>
-												<span>{result.vote_average.toFixed(1)}</span>
-												<StarIcon className='size-3' />
-											</div>
-										</div>
-										<p className='mt-1.5 truncate font-medium'>{result.title}</p>
-										<p className='truncate font-thin text-[0.75rem]'>{result.overview}</p>
-									</Link>
-								</li>
-							))}
-						</div>
-					)}
-				</FocusProvider>
-			</main>
-		</>
+		<main className='py-2 pb-[100px]'>
+			<ul className='mt-4'>
+				{HOME_GENRE_LIST.map((genre, index) => (
+					/* enable track observer except for first 2 tracks that will be in view initially */
+					/* subsequent tracks are fetched and rendered when their observer first enters view (performance optimization) */
+					<Track.Observer key={genre.id} enabled={index > 2}>
+						<HomeTrack
+							key={genre.id}
+							trackIndex={index}
+							genreId={genre.id}
+							genreName={genre.name}
+						/>
+					</Track.Observer>
+				))}
+			</ul>
+		</main>
 	)
 }
 
---
+++ src/screen/home/components/HomeTrack.tsx
@@ -0,0 +1,55 @@
+import { Skeleton } from '@/components/skeleton'
+import { PAGE_SIZE } from '@/constants'
+import { useCategoryMovieListInfiniteQuery } from '@/hook/query'
+import { getMergedCategoryMovieListPagesData } from '@/hook/query/useCategoryMovieListInfiniteQuery/getMergedCategoryMovieListPagesData'
+import { Track } from '@/module/track'
+
+type HomeTrackProps = {
+	trackIndex: number
+	genreId: number
+	genreName: string
+}
+
+const HomeTrack = (props: HomeTrackProps) => {
+	const { trackIndex, genreId, genreName } = props
+
+	const response = useCategoryMovieListInfiniteQuery({ genreId: genreId })
+
+	const mergedData = getMergedCategoryMovieListPagesData({ responseData: response.data })
+
+	const results = mergedData?.results
+
+	const handleItemFocus = (index: number) => {
+		const realIndex = index + 1
+
+		if (realIndex % PAGE_SIZE !== 0) return
+
+		response.fetchNextPage()
+	}
+
+	return (
+		<li className='mb-20'>
+			<Track.Heading>{genreName}</Track.Heading>
+			<Track.Section>
+				<Track.List>
+					{response.isLoading
+						? Array.from({ length: 16 }).map((_, index) => (
+								<Skeleton key={index} className={'h-[348px] min-w-[200px]'} />
+							))
+						: null}
+					{results?.map((result, index) => (
+						<Track.ListItem
+							key={result.id}
+							result={result}
+							trackIndex={trackIndex}
+							listItemIndex={index}
+							onItemFocus={() => handleItemFocus(index)}
+						/>
+					))}
+				</Track.List>
+			</Track.Section>
+		</li>
+	)
+}
+
+export { HomeTrack }
