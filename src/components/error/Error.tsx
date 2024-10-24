17bce3098538ce03bdde827673623d5dbf19b4b4
lovrozagar <lovro.zagar5@gmail.com>
2024-10-24 05:12:57 +0200

    refactor(error): swap h1 to h2

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -14,7 +14,7 @@ const Error = (props: ErrorProps) => {
 
 			<main className='relative top-0 left-0 flex flex-1 items-center justify-center bg-background px-4'>
 				<div className='flex flex-col items-center'>
-					<h1 className='mb-4 font-bold text-4xl text-red-500'>An error ocurred</h1>
+					<h2 className='mb-4 font-bold text-4xl text-red-500'>An error ocurred</h2>
 					<p className='mb-8 text-balance text-center font-roboto text-gray-900 text-lg lg:max-w-[600px] dark:text-gray-300'>
 						{error}
 					</p>
