cc7c1041449209f5b528e24b88c2bd2786a0241f
lovrozagar <lovro.zagar5@gmail.com>
2024-10-25 07:26:23 +0200

    feat(icon): add player icons

--- src/components/icon/svg.tsx
+++ src/components/icon/svg.tsx
@@ -1,9 +1,9 @@
-import type { ComponentPropsWithRef } from 'react'
+import type { ComponentPropsWithRef } from "react"
 
-type SvgProps = ComponentPropsWithRef<'svg'>
+type SvgProps = ComponentPropsWithRef<"svg">
 
 const Svg = (props: SvgProps) => {
-	return <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' {...props} />
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" {...props} />
 }
 
 export type { SvgProps }
